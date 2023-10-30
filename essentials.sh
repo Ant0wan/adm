455d075fbf574557e134a14aa1d1a150a59a5821
Antoine Barthelemy <antoine@linux.com>
2023-10-30 23:54:50 +0100

    update

--- essentials.sh
+++ essentials.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+figlet Linux System Administrator | lolcat
+
+printf "\nip address show\n"
+ip address show
+
+printf "\nip route list\n"
+ip route list | lolcat
+
+#apropos --sections=8 director
+printf "\napropos compiler --sections=1\n"
+apropos compiler --sections=1
+
+printf "\nsystemctl list-dependencies --no-pager\n"
+systemctl list-dependencies --no-pager
+
+printf "\nstat /etc/\n"
+stat /etc/ | lolcat
