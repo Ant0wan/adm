6e796bade0e1f6fecfa37036a411ad99de44398d
Antoine Barthelemy <antoine@linux.com>
2023-11-03 14:30:11 +0100

    test

--- essentials.sh
+++ essentials.sh
@@ -47,5 +47,5 @@ printf "\nfind . -name "e*"\n"
 find . -name "e*" 2>/dev/null
 printf "\nfind /var/ -mmin -5\n"
 find /var/ -mmin -5 2>/dev/null
-printf "\n\n"
 printf "\nmeta changed, content modified\n" | lolcat
+printf "\n\n"
