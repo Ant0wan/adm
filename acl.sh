c5d5b5e8aa339b535a738c202dbfb93cef99c53e
Antoine Barthelemy <antoine@linux.com>
2024-02-13 15:47:05 -0500

    add acl

--- acl.sh
+++ acl.sh
@@ -1,3 +1,8 @@
 sudo setfacl --modify user:aaron:rw examplefile
 # -rw-rw-r--+
 getfacl examplefile
+sudo setfacl --modify mask:r examplefile
+sudo setfacl --modify group:wheel:rw examplefile
+sudo setfacl --modify user:aaron:--- examplefile
+sudo setfacl --remove user:aaron examplefile
+sudo setfacl --remove group:wheel examplefile
